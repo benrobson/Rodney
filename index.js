aa0dc9ec0968981c794bddc1a6a0cdf464ecda84
shadowolf <shadowolfbusiness@gmail.com>
2018-03-01 08:19:02 +1100

    Commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client({disableEveryone: true});
 
 client.on('ready', async () => {
   console.log(`${client.user.username} is online.`);
-  client.user.setActivity('Minecraft')
+  client.user.setActivity('in Development...', {type: 'WORKING'})
 });
 
 client.on('message', async message => {
@@ -15,15 +15,47 @@ client.on('message', async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}hello`){
-    message.channel.send('Hello');
+  if (cmd === `${prefix}report`){
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let reason = args.join(" ").slice(22);
+    if(!user) return message.channel.send('Sorry, I couldn\'t find that user.');
+
+    let embed = new Discord.RichEmbed()
+    .setDescription('Report')
+    .setColor('#15f153')
+    .addField('Reported User', `${user} with ID: ${user.id}`)
+    .addField('Reported By:', `${message.author} with ID: ${message.author.id}`)
+    .addField('Channel:', message.channel)
+    .addField('Time:', message.createdAt)
+    .addField('Reason:', reason);
+
+    return message.channel.send(embed);
+  }
+
+  if (cmd === `${prefix}serverinfo`){
+    let icon = message.guild.iconURL;
+    let embed = new Discord.RichEmbed()
+
+    .setDescription('Guild Information')
+    .setColor('#15f153')
+    .setThumbnail(icon)
+    .addField('Server Name:', message.guild.name)
+    .addField('Created At:', message.guild.createdAt)
+    .addField("You Joined:", message.member.joinedAt)
+    .addField("Total Members:", message.guild.memberCount);
+
+    return message.channel.send(embed);
   }
 
   if (cmd === `${prefix}botinfo`){
+    let icon = client.user.displayAvatarURL;
     let embed = new Discord.RichEmbed()
+
     .setDescription('Bot Information')
     .setColor('#15f153')
-    .addField('Bot Name', client.user.username)
+    .setThumbnail(icon)
+    .addField('Bot Name:', client.user.username)
+    .addField('Created At:', client.user.createdAt);
 
     return message.channel.send(embed);
   }
