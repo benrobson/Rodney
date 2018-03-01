5ac4592c0f87b2d74a0f7f85ef01e2bf3cbdd191
shadowolf <shadowolfbusiness@gmail.com>
2018-03-01 22:15:28 +1100

    New Features

---
+++ commands/report.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) {
+  
+}
+
+module.exports.help = {
+  name: "report"
+}
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
-const package = require('./package.json');
-const fs = require("fs");
-const client = new Discord.Client({disableEveryone: true});
+const Discord = require('discord.js'); // this links to the official Discord npm package
+const config = require('./config.json'); // this links to the config.json file
+const package = require('./package.json'); // this links to the package.json file
+const client = new Discord.Client({disableEveryone: true}); // this is the object for the bot itself
+const fs = require('fs'); // this is the 'File System' that reads all of the commands in the commands folder
+
+fs.readdir('./commands/', (err, files) => {
+  if (err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === 'js')
+  if (jsfile.length <= 0) {
+    console.log('Couldn\'t find commands.');
+    return;
+  }
+})
 
 client.on('ready', async () => {
   console.log(`${client.user.username} is online.`);
-  client.user.setActivity('in Development...', {type: 'PLAYING'})
+  client.user.setActivity('in Development...', {type: 'PLAYING'});
 });
 
 client.on('message', async message => {
@@ -46,8 +55,66 @@ client.on('message', async message => {
     let reportschannel = message.guild.channels.find('name', 'reports');
     if (!reportschannel) return message.channel.send('Sorry, I couldn\'t find the Reports Channel, unable to send this report.');
 
-    return message.channel.send(embed);
+    message.delete().catch(O_o=>{});
+    reportschannel.send(embed);
+    return;
   }
+
+  // Kick Command
+  if (cmd === `${prefix}kick`){
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!user) return message.channel.send('Sorry, I couldn\'t find that user.');
+    let reason = args.join(' ').slice(22);
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Insuffenient Permissions.');
+    if (user.hasPermission('MANAGE_MESSAGES')) return message.channel.send('This user cannot be kicked.');
+
+
+    let embed = new Discord.RichEmbed()
+    .setDescription('**User has been Kicked**')
+    .setColor(config.reportembedcolor)
+    .addField('Kicked User', `${user} with ID: ${user.id}`)
+    .addField('Kicked By:', `${message.author} with ID: ${message.author.id}`)
+    .addField('Kicked in Channel:', message.channel)
+    .addField('Time:', message.createdAt)
+    .addField('Reason:', reason);
+
+    let auditlogchannel = message.guild.channels.find('name', 'audit-log');
+    if (!reportschannel) return message.channel.send('Sorry, I couldn\'t find the Audit Log Channel, unable to send this punishment notification.');
+
+    message.guild.member(user).kick(reason);
+    message.delete().catch(O_o=>{});
+    auditlogchannel.send(embed);
+
+    return;
+    }
+
+    // Ban Command
+    if (cmd === `${prefix}ban`){
+      let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if(!user) return message.channel.send('Sorry, I couldn\'t find that user.');
+      let reason = args.join(' ').slice(22);
+      if (!message.member.hasPermission('MANAGE_MEMBERS')) return message.channel.send('Insuffenient Permissions.');
+      if (user.hasPermission('MANAGE_MEMBERS')) return message.channel.send('This user cannot be banned.');
+
+
+      let embed = new Discord.RichEmbed()
+      .setDescription('**User has been Banned**')
+      .setColor(config.reportembedcolor)
+      .addField('Banned User', `${user} with ID: ${user.id}`)
+      .addField('Banned By:', `${message.author} with ID: ${message.author.id}`)
+      .addField('Banned in Channel:', message.channel)
+      .addField('Time:', message.createdAt)
+      .addField('Reason:', reason);
+
+      let auditlogchannel = message.guild.channels.find('name', 'audit-log');
+      if (!reportschannel) return message.channel.send('Sorry, I couldn\'t find the Audit Log Channel, unable to send this punishment notification.');
+
+      message.guild.member(user).ban(reason);
+      message.delete().catch(O_o=>{});
+      auditlogchannel.send(embed);
+
+      return;
+      }
 
   // Server Information Command
   if (cmd === `${prefix}serverinfo`){
