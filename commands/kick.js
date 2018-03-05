dbac09871c804f1bccbf6d2366ba4a8921736ecf
shadowolf <shadowolfbusiness@gmail.com>
2018-03-05 12:21:37 +1100

    Added not enough permissions error embed.

--- commands/kick.js
+++ commands/kick.js
@@ -5,7 +5,14 @@ module.exports.run = async (client, message, args) => {
   let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if (!user) return message.channel.send('Sorry, I couldn\'t find that user.');
   let reason = args.join(' ').slice(22);
-  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Insuffenient Permissions.');
+  if (!message.member.hasPermission('MANAGE_MESSAGES')){
+    let embed = new Discord.RichEmbed()
+    .setTitle('An error has occurred!')
+    .setColor(config.errorembedcolor)
+    .setDescription('You do not have sufficent permissions to use this command.');
+
+    message.channel.send(embed);
+  };
   if (user.hasPermission('MANAGE_MESSAGES')) return message.channel.send('This user cannot be kicked.');
 
   let embed = new Discord.RichEmbed()
@@ -21,7 +28,7 @@ module.exports.run = async (client, message, args) => {
   if (!auditlogchannel) return message.channel.send('Sorry, I couldn\'t find the Audit Log Channel, unable to send this punishment notification.');
 
   message.guild.member(user).kick(reason);
-  message.delete().catch(O_o=>{});
+  //message.delete().catch(O_o=>{});
   auditlogchannel.send(embed);
 }
 
