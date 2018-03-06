dabc1e7d6c4fb9063a821ccf33968fadafd79945
shadowolf <shadowolfbusiness@gmail.com>
2018-03-06 11:42:52 +1100

    Added help field and added server owner.

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -5,11 +5,12 @@ module.exports.run = async (client, message, args) => {
   let icon = message.guild.iconURL;
   let embed = new Discord.RichEmbed()
 
-  .setDescription(`**Information about ${message.guild.name}**`)
+  .setTitle(`Information about ${message.guild.name}`)
   .setColor(config.plainembedcolor)
   .setThumbnail(icon)
   .addField('Server Name:', message.guild.name)
   .addField('Created At:', message.guild.createdAt)
+  .addField('Server Owner:', message.guild.owner)
   .addField('You Joined:', message.member.joinedAt)
   .addField('Total Members:', message.guild.memberCount);
 
@@ -17,5 +18,7 @@ module.exports.run = async (client, message, args) => {
 }
 
 module.exports.help = {
-  name: 'serverinfo'
+  name: 'serverinfo',
+  description: 'Displays information about the server/guild',
+  usage: 'serverinfo'
 }
