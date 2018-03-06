72c2628126e2f6a71b268a15f8efd8239d546ccf
shadowolf <shadowolfbusiness@gmail.com>
2018-03-06 13:53:01 +1100

    Added new command with limited fields.

---
+++ commands/userinfo.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js'); // this links to the official Discord npm package
+const config = require('../config.json'); // this links to the config.json file
+
+module.exports.run = async (client, message, args) => {
+  let icon = message.client.avatarURL;
+
+  let embed = new Discord.RichEmbed()
+  .setTitle(`Information about ${message.author.username}`)
+  .setColor(config.plainembedcolor)
+  .setThumbnail(icon)
+  .addField('Username:', client.user.name)
+  .addField('You Joined Guild at:', message.member.joinedAt);
+
+  return message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: 'userinfo',
+  description: 'Displays information about the user mentioned',
+  usage: 'userinfo'
+}
