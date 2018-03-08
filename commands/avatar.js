377356515fce02a6ed7e542b667a10e7b332a166
shadowolf <shadowolfbusiness@gmail.com>
2018-03-08 23:21:08 +1100

    Adding avatar command.

---
+++ commands/avatar.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js'); // this links to the official Discord npm package
+const config = require('../config.json'); // this links to the config.json file
+
+module.exports.run = async (client, message, args) => {
+/*  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if (!user){
+    let embed = new Discord.RichEmbed()
+    .setTitle('An error has occurred!')
+    .setColor(config.errorembedcolor)
+    .setDescription('This user could not be found, or does not exist.');
+    message.channel.send(embed);
+    return
+  };*/
+
+  let embed = new Discord.RichEmbed()
+  .setTitle('Here is your avatar!')
+  .setColor(config.plainembedcolor)
+  .setImage(`${message.author.avatarURL}`);
+  message.channel.send(embed);
+  return
+
+};
+
+  module.exports.help = {
+    name: 'avatar',
+    description: 'This will display your avatar.',
+    usage: 'avatar'
+  }
