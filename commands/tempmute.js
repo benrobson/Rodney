15cc792e9c2d10997cea66e219bcb7fbecdd97e0
shadowolf <shadowolfbusiness@gmail.com>
2018-03-08 23:13:23 +1100

    Commiting for assistance.

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -5,21 +5,22 @@ const ms = require('ms'); // this package allows us to use time
 module.exports.run = async (client, message, args) => {
 
   let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if (user.hasPermission('MANAGE_MESSAGES')){
+      const embed = new Discord.RichEmbed()
+      .setTitle('An error has occurred!')
+      .setColor(config.errorembedcolor)
+      .setDescription('You do not have permission to use this command.');
+      message.channel.send(embed)
+      return
+  };
   if (!user) {
     const embed = new Discord.RichEmbed()
     .setTitle('An error has occurred!')
     .setColor(config.errorembedcolor)
     .setDescription('The user you would like to Temporarily Mute could not be found.');
     message.channel.send(embed);
-    message.delete().catch(O_o=>{});
-  };
-  if (user.hasPermission('MANAGE_MESSAGES')){
-      const embed = new Discord.RichEmbed()
-      .setTitle('An error has occurred!')
-      .setColor(config.errorembedcolor)
-      .setDescription('You do not have permission to use this command.');
-      message.channel.send(embed);
-      message.delete().catch(O_o=>{});
+    message.delete().catch(O_o=>{})
+    return
   };
   let reason = args.join(' ').slice(2).join(' ');
   if (!reason) {
@@ -28,7 +29,8 @@ module.exports.run = async (client, message, args) => {
     .setColor(config.errorembedcolor)
     .setDescription('Please supply a reason for the Temporary Mute.');
     message.channel.send(embed);
-    message.delete().catch(O_o=>{});
+    message.delete().catch(O_o=>{})
+    return
   };
 
   let muterole = message.guild.roles.find('name', 'Muted'); // Bot checks to see if there is a role named Muted.
@@ -48,7 +50,7 @@ module.exports.run = async (client, message, args) => {
     } catch(e) {
       console.log(e.stack);
     }
-  }
+  };
 
   let time = args[1];
   if (!time) return message.reply('you have not specified a time.');
@@ -61,7 +63,7 @@ module.exports.run = async (client, message, args) => {
     .setColor(config.plainembedcolor)
     .addField('Muted User', `${user} with ID: ${user.id}`)
     auditlogchannel.send(embed);
-  }, ms(time))
+  }, ms(time));
 
   let embed = new Discord.RichEmbed()
   .setTitle('User has been Temporarily Muted')
@@ -76,9 +78,12 @@ module.exports.run = async (client, message, args) => {
   if (!auditlogchannel) return message.channel.send('Sorry, I couldn\'t find the Audit Log Channel, unable to send this punishment notification.');
 
   message.delete().catch(O_o=>{});
-  auditlogchannel.send(embed);
+  auditlogchannel.send(embed)
+  return
 };
 
 module.exports.help = {
-  name: 'tempmute'
-}
+  name: 'tempmute',
+  description: 'This will temporarily mute a user',
+  usage: 'tempmute [user] [time s/m/h/d] [reason]'
+};
