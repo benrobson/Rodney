5f233fdf8f0ad7398b0c00f35f95472711d46c6c
shadowolf <shadowolfbusiness@gmail.com>
2018-03-09 12:19:47 +1100

    Fixed the issue allowing poll command to create empty polls.

--- commands/poll.js
+++ commands/poll.js
@@ -2,6 +2,15 @@ const Discord = require('discord.js'); // this links to the official Discord npm
 const config = require('../config.json'); // this links to the config.json file
 
 module.exports.run = async (client, message, args) => {
+  if (args == 0){
+    let embed = new Discord.RichEmbed()
+    .setTitle('An error has occurred!')
+    .setColor(config.errorembedcolor)
+    .setDescription('You cannot send an empty poll.');
+    message.channel.send(embed);
+    return
+  };
+
   let embed = new Discord.RichEmbed()
   .setTitle(`Poll by ${message.author.username}`)
   .setColor(config.plainembedcolor)
@@ -10,14 +19,14 @@ module.exports.run = async (client, message, args) => {
   return message.channel.send(embed).then(message.delete())
 
   .then(function (message, str) {
-        message.react("👍")
-        message.react("👎")
-      }).catch(function() {
+       message.react("👍")
+       message.react("👎")
+     }).catch(function() {
   });
-}
+};
 
 module.exports.help = {
   name: 'poll',
   description: 'Displays information about the user mentioned',
   usage: 'poll [question]'
-}
+};
