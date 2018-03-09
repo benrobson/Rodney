da89e4723c789f4ab1246e1e40226eec0761a30b
shadowolf <shadowolfbusiness@gmail.com>
2018-03-09 15:30:06 +1100

    Added say command.

---
+++ commands/say.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js'); // this links to the official Discord npm package
+const config = require('../config.json'); // this links to the config.json file
+
+module.exports.run = async (client, message, args) => {
+  if (!message.member.hasPermission('MANAGE_MESSAGES')){
+    let embed = new Discord.RichEmbed()
+    .setTitle('An error has occurred!')
+    .setColor(config.errorembedcolor)
+    .setDescription('You do not have sufficent permissions to use this command.');
+    message.channel.send(embed);
+    return
+  };
+  if (!args[0]){
+    let embed = new Discord.RichEmbed()
+    .setTitle('An error has occurred!')
+    .setColor(config.errorembedcolor)
+    .setDescription('Please specify the message you would like to send, you cannot send an empty message.');
+    message.channel.send(embed);
+    return
+  };
+
+  let inputmessage = args.join(' ');
+  let embed = new Discord.RichEmbed()
+  .setTitle(`Message from ${message.author.username}`)
+  .setColor(config.joinembedcolor)
+  .setDescription(inputmessage)
+  message.delete().catch(O_o=>{});
+  message.channel.send(embed);
+  return
+};
+
+module.exports.help = {
+  name: 'say',
+  description: 'This will send a message as the bot on your behalf.',
+  usage: 'say [message]'
+};
