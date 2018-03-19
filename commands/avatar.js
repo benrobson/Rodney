aceba2c9ef6673e6ee289d429f9c23a4c03d5abd
shadowolf <shadowolfbusiness@gmail.com>
2018-03-19 16:44:05 +1100

    Fixed mentioned avatars

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,23 +2,14 @@ const Discord = require('discord.js'); // this links to the official Discord npm
 const config = require('../config.json'); // this links to the config.json file
 
 module.exports.run = async (client, message, args) => {
-/*  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if (!user){
-    let embed = new Discord.RichEmbed()
-    .setTitle('An error has occurred!')
-    .setColor(config.errorembedcolor)
-    .setDescription('This user could not be found, or does not exist.');
-    message.channel.send(embed);
-    return
-  };*/
+  let user = message.mentions.users.first();
 
   let embed = new Discord.RichEmbed()
-  .setTitle('Here is your avatar!')
+  .setTitle(`Here is the Users Avatar`)
   .setColor(config.plainembedcolor)
-  .setImage(`${message.author.avatarURL}`);
+  .setImage(user.avatarURL);
   message.channel.send(embed);
   return
-
 };
 
   module.exports.help = {
