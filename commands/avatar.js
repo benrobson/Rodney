6e654f1bb23b4ec1aa8f4513813edcaf9340a37b
shadowolf <shadowolfbusiness@gmail.com>
2018-03-19 16:45:26 +1100

    Added invalid user error.

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,6 +3,14 @@ const config = require('../config.json'); // this links to the config.json file
 
 module.exports.run = async (client, message, args) => {
   let user = message.mentions.users.first();
+  if (!user){
+    let embed = new Discord.RichEmbed()
+    .setTitle('An error has occurred!')
+    .setColor(config.errorembedcolor)
+    .setDescription('This user could not be found, or does not exist.');
+    message.channel.send(embed);
+    return
+  };
 
   let embed = new Discord.RichEmbed()
   .setTitle(`Here is the Users Avatar`)
