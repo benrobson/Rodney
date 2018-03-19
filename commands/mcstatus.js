5ab06ca7dd1aa07d66b19481205f5194dd43f6ea
shadowolf <shadowolfbusiness@gmail.com>
2018-03-19 16:57:17 +1100

    Added an in progress Minecraft Server command.

---
+++ commands/mcstatus.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js'); // this links to the official Discord npm package
+const config = require('../config.json'); // this links to the config.json file
+const superagent = require('superagent');
+
+module.exports.run = async (client, message, args) => {
+  const mcIP = args.join(' ').slice(12);
+
+  let {body} = await superagent
+  .get('http://mcapi.us/server/status?ip=' + mcIP);
+
+  let embed = new Discord.RichEmbed()
+  .setTitle(`Information about this Minecraft Server.`)
+  .setColor(config.plainembedcolor)
+  .addField('Server Online:', body.online, true)
+  .addField('Players On:', body.players.now, true)
+  .addField('Max Players:', body.players.max, true);
+
+  return message.channel.send(embed);
+};
+
+module.exports.help = {
+  name: 'mcstatus',
+  description: 'Displays information about the Minecraft Server.',
+  usage: 'mcstatus [server ip]'
+};
