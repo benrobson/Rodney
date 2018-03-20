a58f4fd226886ad91e2ca560f788d8c4b2ddd146
shadowolf <shadowolfbusiness@gmail.com>
2018-03-20 20:01:44 +1100

    Expanding swear dictionary.

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ client.on('message', async message => {
     };
 
     // Swear Detector
-    const swearWords = ['shit', 'fuck', 'bitch', 'nigger', 'nigga', 'cunt', 'whore'];
+    const swearWords = ['shit', 'fuck', 'bitch', 'nigger', 'nigga', 'cunt', 'whore', 'fag', 'faggot', 'dick', 'cock', 'pussy', 'slut', 'bastard'];
     if(swearWords.some(word => message.content.toLowerCase().includes(word))) {
       message.delete().catch(O_o=>{});
 
