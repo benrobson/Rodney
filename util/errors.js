c5016e0921c11ddf8513a2a991c631ebe5a4b6ea
shadowolf <shadowolfbusiness@gmail.com>
2018-03-22 19:39:27 +1100

    Added error embeds.

---
+++ util/errors.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js'); // this links to the official Discord npm package
+const config = require('../config.json'); // this links to the config.json file
+
+module.exports.noLogChannel = (message, perm) => {
+  let embed = new Discord.RichEmbed()
+  .setTitle('An error has occurred!')
+  .setColor(config.red)
+  .setDescription('Sorry, I could not find the Audit Log Channel. This notification was unable to be sent!');
+
+  message.channel.send(embed).then(message => message.delete(config.errortimeout));
+};
+
+module.exports.noPermissions = (message, perm) => {
+  let embed = new Discord.RichEmbed()
+  .setTitle('An error has occurred!')
+  .setDescription(`You have insufficent permissions to run this command. You require the permission flag of **${perm}**!`)
+  .setColor(config.red)
+
+  message.channel.send(embed).then(message => message.delete(config.errortimeout));
+};
+
+module.exports.invalidUser = (message) => {
+  let embed = new Discord.RichEmbed()
+  .setTitle('An error has occurred!')
+  .setDescription('This user could not be found or does not exist!')
+  .setColor(config.red);
+
+  message.channel.send(embed).then(message => message.delete(config.errortimeout));
+};
