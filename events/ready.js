2f19de6ff66856a232a1ed048b07083cf63432a7
shadowolf <shadowolfbusiness@gmail.com>
2018-03-29 15:57:20 +1100

    Removing Event Loader...

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = client => {
-  let pluralnonpluralservers = (client.guilds.size > 1) ? 'Servers' : 'Server';
-  let pluralnonpluralusers = (client.users.size > 1) ? 'Users' : 'User';
-  console.log(`${client.user.username} is online and is operating on ${client.guilds.size} ${pluralnonpluralservers} for ${client.users.size} ${pluralnonpluralusers}.`);
-  client.user.setActivity(`${client.guilds.size} ${pluralnonpluralservers} // ${client.users.size} ${pluralnonpluralusers}`, {type: 'PLAYING'});
-  return
-}
