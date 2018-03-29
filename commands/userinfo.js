d661f3197f2902fe49322b367f9fb8ed18908f16
shadowolf <shadowolfbusiness@gmail.com>
2018-03-29 21:14:19 +1100

    Added new field to the command.

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -6,21 +6,37 @@ module.exports.run = async (client, message, args) => {
   let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if (!user) return errors.invalidUser(message);
 
-  let kickable = message.member.kickable ? "✅" : "❎";
-  let bannable = message.member.bannable ? "✅" : "❎";
-  let icon = user.displayAvatarURL;
+  let kickable = user.kickable ? "✅" : "❎";
+  let bannable = user.bannable ? "✅" : "❎";
+  let icon = user.user.displayAvatarURL;
+
+  let nickname = user.nickname;
+  if (!nickname) {
+    nickname = user.user.username;
+  };
+
+  let createdAtRaw = user.user.createdAt.toDateString();
+  let createdAt = createdAtRaw.split(" ");
+  let joinedAtRaw = user.joinedAt.toDateString();
+  let joinedAt = joinedAtRaw.split(" ");
+
+  let playingStatus = user.presence.game;
+  if (playingStatus) {
+    playingStatus = user.presence.game.name;
+  } else {
+    playingStatus = 'None'
+  }
 
   let embed = new Discord.RichEmbed()
-  .setTitle(`Information about ${message.author.username}`)
+  .setTitle(`Information about ${nickname}`)
   .setColor(config.red)
   .setThumbnail(icon)
-  .addField('Username', message.username, true)
+  .addField('Username', user.user.tag, true)
   .addField('User ID', user.id, true)
   .addField('Status', user.presence.status, true)
-  .addField('Roles:', '*Coming Soon*')
-  .addField('Messages Sent:', '*Coming Soon*')
-  .addField('Account Created at:', '*Coming Soon*')
-  .addField('You Joined:', message.member.joinedAt)
+  .addField('Playing Status', playingStatus, true)
+  .addField('Account Created On', `${createdAt[0]} ${createdAt[2]} ${createdAt[1]} ${createdAt[3]}`, true)
+  .addField('Joined This Guild On', `${joinedAt[0]} ${joinedAt[2]} ${joinedAt[1]} ${joinedAt[3]}`, true)
   .addField('Bannable', bannable, true)
   .addField('Kickable', kickable, true);
   message.channel.send(embed);
