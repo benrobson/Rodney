3c330ca8d4bb9ec6da18504c36c27b494adeddca
shadowolf <shadowolfbusiness@gmail.com>
2018-03-29 20:58:16 +1100

    Added additional fields.

--- commands/guildinfo.js
+++ commands/guildinfo.js
@@ -2,22 +2,44 @@ const Discord = require('discord.js'); // this links to the official Discord npm
 const config = require('../config.json'); // this links to the config.json file
 
 module.exports.run = async (client, message, args) => {
+  let guild = message.guild;
   let large = message.guild.large ? "✅" : "❎";
   let icon = message.guild.iconURL;
+
+  let textChannels = 0;
+  let voiceChannels = 0;
+  guild.channels.forEach(channel => {
+  channel.type === "text" ? textChannels++ : voiceChannels++;
+  });
+
+  let emojis = [];
+  guild.emojis.forEach(emoji => {
+  emojis.push(`\`${emoji.name}\``);
+  });
+  emojis.length === 0 ? emojis = "None" : emojis = emojis.join(", ");
+
+  let roles = [];
+  guild.roles.forEach(role => {
+    roles.push(`\`${role.name}\``);
+  });
+  roles = roles.join(", ");
 
   let embed = new Discord.RichEmbed()
   .setTitle(`Information about ${message.guild.name}`)
   .setColor(config.white)
   .setThumbnail(icon)
-  .addField('Server Name', message.guild.name, true)
-  .addField('Server Owner', message.guild.owner, true)
-  .addField('Created At', message.guild.createdAt)
-  .addField('Server Region', message.guild.region, true)
-  .addField('Total Members:', message.guild.memberCount, true)
+  .addField('Guild Name', guild.name, true)
+  .addField('Guild ID', guild.id, true)
+  .addField('Guild Owner', guild.owner, true)
+  .addField('Created At', guild.createdAt)
+  .addField('Server Region', guild.region, true)
+  .addField('Total Members:', guild.memberCount, true)
   .addField('Large', large, true)
-  .addField('Verification Level', message.guild.verificationLevel, true)
-  .addField('List of Channels', message.guild.channels.size)
-  .addField('List of Roles', message.guild.verificationLevel);
+  .addField('Verification Level', guild.verificationLevel, true)
+  .addField('Text Channels', textChannels, true)
+  .addField('Text Channels', voiceChannels, true)
+  .addField('Roles', `${guild.roles.name}`, true)
+  .addField('Emojis', `${guild.emojis.size}`, true)
 
   return message.channel.send(embed);
 }
