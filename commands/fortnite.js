f25ccd2ddf8db1902e803233d616f55bae4a8663
shadowolf <shadowolfbusiness@gmail.com>
2018-04-26 15:59:32 +1000

    Adding finishing touches.

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -3,6 +3,7 @@ const { red, purple } = require('../config.json');
 const { fortniteapi } = require('../token.json');
 const { Client } = require('fortnite');
 const stats = new Client(fortniteapi);
+const errors = require('../util/errors.js');
 
 module.exports.run = async (client, message, args, tools) => {
 	if (args[0] === undefined || args[1] === undefined) {
@@ -12,11 +13,7 @@ module.exports.run = async (client, message, args, tools) => {
 			.setDescription(`Please include a platform and username in your arguments.`)
 		return message.channel.send(embed)
 	} else if (/(pc|xbl|psn)/.test(args[0].toLowerCase()) === false) {
-		const embed = new RichEmbed()
-			.setTitle(`:warning: Error :warning:`)
-			.setColor(red)
-			.setDescription(`Invalid platform, try using PC, XBL, or PSN`)
-		return message.channel.send(embed);
+		return errors.invalidPlatform(message);
 	} else {
 		const platform = args[0].toLowerCase().match(/(pc|xbl|psn)/)[0];
 		const username = args.slice(1).join(' ');
@@ -27,47 +24,14 @@ module.exports.run = async (client, message, args, tools) => {
 				.setTitle(`Stats of ${data.username} | Platform: ${data.platform}`)
 				.setURL(data.url)
 				.setThumbnail('https://d1u5p3l4wpay3k.cloudfront.net/fortnite_gamepedia/6/64/Favicon.ico')
-				.setDescription(`
-Solo Info
----------------
-Score/Kills: ${data.stats.solo.score}/${data.stats.solo.kills}
-KD: ${data.stats.solo.kills}
-Scores/Kills Per Match: ${data.stats.solo.kills_per_match}/${data.stats.solo.score_per_match}
-Matches/Wins: ${data.stats.solo.matches}/${data.stats.solo.wins}
-Top 3s: ${data.stats.solo.top_3}
-Top 5s: ${data.stats.solo.top_5}
-Top 6s: ${data.stats.solo.top_6}
-Top 12s: ${data.stats.solo.top_12}
-Top 25s: ${data.stats.solo.top25} 
-
-Duo Info
----------------
-Score/Kills: ${data.stats.duo.score}/${data.stats.duo.kills}
-KD: ${data.stats.duo.kills}
-Scores/Kills Per Match: ${data.stats.duo.kills_per_match}/${data.stats.duo.score_per_match}
-Matches/Wins: ${data.stats.duo.matches}/${data.stats.duo.wins}
-Top 3s: ${data.stats.duo.top_3}
-Top 5s: ${data.stats.duo.top_5}
-Top 6s: ${data.stats.duo.top_6}
-Top 12s: ${data.stats.duo.top_12}
-Top 25s: ${data.stats.duo.top25}
-
-Squad Info
----------------
-Score/Kills: ${data.stats.squad.score}/${data.stats.squad.kills}
-KD: ${data.stats.squad.kills}
-Scores/Kills Per Match: ${data.stats.squad.kills_per_match}/${data.stats.squad.score_per_match}
-Matches/Wins: ${data.stats.squad.matches}/${data.stats.squad.wins}
-Top 3s: ${data.stats.squad.top_3}
-Top 5s: ${data.stats.squad.top_5}
-Top 6s: ${data.stats.squad.top_6}
-Top 12s: ${data.stats.squad.top_12}
-Top 25s: ${data.stats.squad.top25} 
-				`)
+				.addField('Solo Info', `Score/Kills: ${data.stats.solo.score}/${data.stats.solo.kills}\nKD: ${data.stats.solo.kills}\nMatches/Wins: ${data.stats.solo.matches}/${data.stats.solo.wins}\nTop 3s: ${data.stats.solo.top_3}\nTop 5s: ${data.stats.solo.top_5}\nTop 6s: ${data.stats.solo.top_6}\nTop 12s: ${data.stats.solo.top_12}\nTop 25s: ${data.stats.solo.top_25}`)
+				.addField('Duo Info', `Score/Kills: ${data.stats.duo.score}/${data.stats.duo.kills}\nKD: ${data.stats.duo.kills}\nScores/Kills Per Match: ${data.stats.duo.kills_per_match}/${data.stats.duo.score_per_match}\nMatches/Wins: ${data.stats.duo.matches}/${data.stats.duo.wins}\nTop 3s: ${data.stats.duo.top_3}\nTop 5s: ${data.stats.duo.top_5}\nTop 6s: ${data.stats.duo.top_6}\nTop 12s: ${data.stats.duo.top_12}\nTop 25s: ${data.stats.duo.top_25}`)
+				.addField('Squad Info', `Score/Kills: ${data.stats.squad.score}/${data.stats.squad.kills}\nKD: ${data.stats.squad.kills}\nScores/Kills Per Match: ${data.stats.squad.kills_per_match}/${data.stats.squad.score_per_match}\nMatches/Wins: ${data.stats.squad.matches}/${data.stats.squad.wins}\nTop 3s: ${data.stats.squad.top_3}\nTop 5s: ${data.stats.squad.top_5}\nTop 6s: ${data.stats.squad.top_6}\nTop 12s: ${data.stats.squad.top_12}\nTop 25s: ${data.stats.squad.top_25}`)
 				.setColor(purple)
 			return message.channel.send(embed);
 		} catch (error) {
-			return message.reply(`Invalid User...`);
+			console.log(error);
+			return errors.invalidUser(message);
 		}
 	}
 };
