68669388dea6a0cadf2277a774edd5cabe458aa8
GitStonic <kingstonicthe1@gmail.com>
2018-04-26 01:08:06 -0400

    fixed dumbass shit

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -30,39 +30,39 @@ module.exports.run = async (client, message, args, tools) => {
 				.setDescription(`
 Solo Info
 ---------------
-Score/Kills: ${data.solo.score}/${data.solo.kills}
-KD: ${data.solo.kills}
-Scores/Kills Per Match: ${data.solo.kills_per_match}/${data.solo.score_per_match}
-Matches/Wins: ${data.solo.matches}/${data.solo.wins}
-Top 3s: ${data.solo.top_3}
-Top 5s: ${data.solo.top_5}
-Top 6s: ${data.solo.top_6}
-Top 12s: ${data.solo.top_12}
-Top 25s: ${data.solo.top25} 
+Score/Kills: ${data.stats.solo.score}/${data.stats.solo.kills}
+KD: ${data.stats.solo.kills}
+Scores/Kills Per Match: ${data.stats.solo.kills_per_match}/${data.stats.solo.score_per_match}
+Matches/Wins: ${data.stats.solo.matches}/${data.stats.solo.wins}
+Top 3s: ${data.stats.solo.top_3}
+Top 5s: ${data.stats.solo.top_5}
+Top 6s: ${data.stats.solo.top_6}
+Top 12s: ${data.stats.solo.top_12}
+Top 25s: ${data.stats.solo.top25} 
 
 Duo Info
 ---------------
-Score/Kills: ${data.duo.score}/${data.duo.kills}
-KD: ${data.duo.kills}
-Scores/Kills Per Match: ${data.duo.kills_per_match}/${data.duo.score_per_match}
-Matches/Wins: ${data.duo.matches}/${data.duo.wins}
-Top 3s: ${data.duo.top_3}
-Top 5s: ${data.duo.top_5}
-Top 6s: ${data.duo.top_6}
-Top 12s: ${data.duo.top_12}
-Top 25s: ${data.duo.top25}
+Score/Kills: ${data.stats.duo.score}/${data.stats.duo.kills}
+KD: ${data.stats.duo.kills}
+Scores/Kills Per Match: ${data.stats.duo.kills_per_match}/${data.stats.duo.score_per_match}
+Matches/Wins: ${data.stats.duo.matches}/${data.stats.duo.wins}
+Top 3s: ${data.stats.duo.top_3}
+Top 5s: ${data.stats.duo.top_5}
+Top 6s: ${data.stats.duo.top_6}
+Top 12s: ${data.stats.duo.top_12}
+Top 25s: ${data.stats.duo.top25}
 
 Squad Info
 ---------------
-Score/Kills: ${data.squad.score}/${data.squad.kills}
-KD: ${data.squad.kills}
-Scores/Kills Per Match: ${data.squad.kills_per_match}/${data.squad.score_per_match}
-Matches/Wins: ${data.squad.matches}/${data.squad.wins}
-Top 3s: ${data.squad.top_3}
-Top 5s: ${data.squad.top_5}
-Top 6s: ${data.squad.top_6}
-Top 12s: ${data.squad.top_12}
-Top 25s: ${data.squad.top25} 
+Score/Kills: ${data.stats.squad.score}/${data.stats.squad.kills}
+KD: ${data.stats.squad.kills}
+Scores/Kills Per Match: ${data.stats.squad.kills_per_match}/${data.stats.squad.score_per_match}
+Matches/Wins: ${data.stats.squad.matches}/${data.stats.squad.wins}
+Top 3s: ${data.stats.squad.top_3}
+Top 5s: ${data.stats.squad.top_5}
+Top 6s: ${data.stats.squad.top_6}
+Top 12s: ${data.stats.squad.top_12}
+Top 25s: ${data.stats.squad.top25} 
 				`)
 				.setColor(purple)
 			return message.channel.send(embed);
