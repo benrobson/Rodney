fabf145a43c1fd4fd27a3df61917be2801a58c6e
shadowolf <shadowolfbusiness@gmail.com>
2018-05-07 19:49:02 +1000

    Added reload command.

---
+++ commands/reload.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const errors = require('../util/errors.js');
+
+module.exports.run = async (client, message, args) => {
+  if (message.author.id === config.ownerid) {
+    if (!args || args.size < 1) return errors.noCommandName(message);
+    delete require.cache[require.resolve(`../commands/${args[0]}.js`)];
+
+    let embed = new Discord.RichEmbed()
+    .setTitle('Command has been reloaded.')
+    .setDescription(`The ${args[0]} command has been reloaded.`)
+    .setColor(config.green);
+
+    message.channel.send(embed);
+  } else return errors.ownerOnly(message);
+};
+
+module.exports.help = {
+    name: 'reload',
+    description: '',
+    usage: 'reload [command]'
+};
