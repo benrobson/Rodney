fba2ffefdbfbd61785f74e679646a9c4cd4982f1
shadowolf <shadowolfbusiness@gmail.com>
2018-05-08 08:22:32 +1000

    Added a setActivity rotator.

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,18 @@
+const config = require('../config.json');
+
 module.exports = client => {
   let pluralnonpluralservers = (client.guilds.size > 1) ? 'Servers' : 'Server';
   let pluralnonpluralusers = (client.users.size > 1) ? 'Users' : 'User';
 
   console.log(`${client.user.username} is online and is operating on ${client.guilds.size} ${pluralnonpluralservers} for ${client.users.size} ${pluralnonpluralusers}.`);
-  client.user.setActivity(`${client.guilds.size} ${pluralnonpluralservers} // ${client.users.size} ${pluralnonpluralusers}`, {type: 'PLAYING'});
-  return
+
+function setActivity() {
+  const Gameinfo = [`Running on ${client.guilds.size} ${pluralnonpluralservers}`, `Running for ${client.users.size} ${pluralnonpluralusers}`, `Use ${config.prefix}help for help`, `Using ${(((process.memoryUsage().heapUsed)/1024)/1024).toFixed(0)}MBs of RAM`];
+  var info = Gameinfo[Math.floor(Math.random() * Gameinfo.length)];
+
+  client.user.setActivity(info);
+  console.log(`[ LOG ] Activity set to (${info})`);
+}
+
+setInterval(setActivity, 120000);
 }
--- index.js
+++ index.js
@@ -77,4 +77,5 @@ client.on('message', (message) => {
   };
 });
 
+
 client.login(token.token);
