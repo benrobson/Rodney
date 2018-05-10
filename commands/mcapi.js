79bc29a2c95db9b323cf8153a1c9663c210ea365
shadowolf <shadowolfbusiness@gmail.com>
2018-05-10 17:54:37 +1000

    Updating mcapi command and adding request package.

--- commands/mcapi.js
+++ commands/mcapi.js
@@ -1,23 +1,34 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
 const superagent = require('superagent');
+const request = require('request');
 
 module.exports.run = async (client, message, args) => {
-  let {body} = await superagent
-  .get('https://status.mojang.com/check');
+  const url = "https://status.mojang.com/check"
 
-  let embed = new Discord.RichEmbed()
-  .setTitle(`Mojang Service Status`)
-  .addField('minecraft.net', , true)
-  .addField('session.minecraft.net', , true)
-  .addField('account.mojang.com', , true)
-  .addField('authserver.mojang.com', , true)
-  .addField('sessionserver.mojang.com', , true)
-  .addField('api.mojang.com', , true)
-  .addField('textures.minecraft.net', , true)
-  .addField('mojang.com', , true)
+    request(url, function (err, response, body) {
+      if (err) {
+        console.log(err);
+        return message.reply('Sorry, cannot get Mojang status at this time.');
+      }
 
-  return message.channel.send(embed);
+      body = JSON.parse(body);
+      for (status in body) {
+        const domain = Object.keys(body[status])[0]
+        const connected = body[status][domain];
+      }
+
+      const embed = new Discord.RichEmbed().addField(`${Object.keys(body[0])[0]}`,`${body[0]['minecraft.net'] ? 'Up And Online! :white_check_mark: ' : 'Sorry, But this site appears to be down :x:'}`)//Minecraft.net
+      .addField(`${Object.keys(body[1])[0]}`,`${body[1]['session.minecraft.net'] ? 'Up And Online! :white_check_mark: ' : 'Sorry, But this site appears to be down :x:'}`)//session.minecraft.net
+      .addField(`${Object.keys(body[2])[0]}`,`${body[2]['account.mojang.com'] ? 'Up And Online! :white_check_mark: ' : 'Sorry, But this site appears to be down :x:'}`)//account.mojang.com
+      .addField(`${Object.keys(body[3])[0]}`,`${body[3]['authserver.mojang.com'] ? 'Up And Online! :white_check_mark: ' : 'Sorry, But this site appears to be down :x:'}`)//authserver.mojang.com
+      .addField(`${Object.keys(body[4])[0]}`,`${body[4]['sessionserver.mojang.com'] ? 'Up And Online! :white_check_mark: ' : 'Sorry, But this site appears to be down :x:'}`)//sessionserver.mojang.com
+      .addField(`${Object.keys(body[5])[0]}`,`${body[5]['api.mojang.com'] ? 'Up And Online! :white_check_mark: ' : 'Sorry, But this site appears to be down :x:'}`)//api.mojang.com
+      .addField(`${Object.keys(body[6])[0]}`,`${body[6]['textures.minecraft.net'] ? 'Up And Online! :white_check_mark: ' : 'Sorry, But this site appears to be down :x:'}`)//textures.minecraft.net
+      .addField(`${Object.keys(body[7])[0]}`,`${body[7]['mojang.com'] ? 'Up And Online! :white_check_mark: ' : 'Sorry, But this site appears to be down :x:'}`)//mojang.com
+      .setColor(config.green)
+      message.channel.send(embed);
+  });
 };
 
 module.exports.help = {
