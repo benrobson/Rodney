e95843af955afe390eb10b830d82a125144a810e
shadowolf <shadowolfbusiness@gmail.com>
2018-05-21 15:14:46 +1000

    Updated this command a bit.

--- commands/issues.js
+++ commands/issues.js
@@ -5,6 +5,8 @@ const package = require('../package.json');
 module.exports.run = async (client, message, args) => {
   let embed = new Discord.RichEmbed()
   .setTitle('Issue Tracker')
+  .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2000px-Octicons-mark-github.svg.png')
+  .setURL(package.bugs.url)
   .setColor(config.white)
   .setDescription(`If you would like to report an issue or make a suggestion in the bot development, please do so on the tracker ${package.bugs.url}`)
 
@@ -14,6 +16,5 @@ module.exports.run = async (client, message, args) => {
 
 module.exports.help = {
   name: 'issues',
-  description: 'Displays information that takes user to the issue tracker.',
-  usage: 'issues'
+  description: 'Displays information that takes user to the issue tracker.'
 }
