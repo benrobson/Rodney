4adfaf08212f26a8b2274573ac1f4ae11a388c9e
shadowolf <shadowolfbusiness@gmail.com>
2018-08-29 18:46:03 +1000

    Updating with omsg and nmsg.

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
 
-module.exports = (message) => {
+module.exports = (omsg, nmsg) => {
   let auditlogchannel = message.guild.channels.find('name', 'audit-log');
   if (!auditlogchannel) return;
 
   let embed = new Discord.RichEmbed()
-  .addField(`Message Deleted in ${message.channel.name}.`, `${message.author.username}: ${message.content}`)
+  .addField(`Message Edited in ${message.channel.name}.`, `${message.author.username}:\n Old Message: ${omsg.content}\n ${nmsg.content}`)
   auditlogchannel.send(embed);
 
   console.log(`[${message.guild}] A message sent by ${message.author.username} has been deleted: ${message.content}.`);
