039a33147d1f02c10153bf7e9240b445ad19a9cd
shadowolfyt <shadowolfbusiness@gmail.com>
2018-08-29 14:07:51 +1000

    Testing a new command.

---
+++ commands/tag.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const errors = require('../util/errors.js');
+
+module.exports.run = async (client, message, args) => {
+  if (!args) {
+    message.channel.send('Where are the args?')
+  }
+
+  if (args[1] == 'xd'){
+    message.channel.send("CAN WE GET AN XD IN THE CHAT BOIS");
+  } else if (args[1] == 'sad') {
+    message.channel.send("Guys, this is so sad, can we get 10 likes.");
+  }
+};
