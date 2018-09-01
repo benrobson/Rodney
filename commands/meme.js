91dd8a1b8e9770fe39f32512f7b9bf16b942105e
shadowolf <shadowolfbusiness@gmail.com>
2018-09-01 13:29:41 +1000

    Updated packages and added memejsfork package.

--- commands/meme.js
+++ commands/meme.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
-const meme = require('memejs');
+const meme = require('memejsfork');
 
 module.exports.run = async (client, message, args) => {
   if (args == 'help') {
@@ -15,8 +15,10 @@ module.exports.run = async (client, message, args) => {
 
   meme(function(data) {
     const embed = new Discord.RichEmbed()
-    .setTitle(data.title[0])
-    .setImage(data.url[0])
+      .setTitle(data.title[0])
+      .setURL(data.url[0])
+      .setDescription(`From r/${data.subreddit[0]} by ${data.author[0]}`)
+      .setImage(data.url[0])
     message.channel.send({embed});
   });
 };
