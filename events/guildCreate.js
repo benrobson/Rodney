53cdade4ddb5296a478118b2979678bba18da334
shadowolf <shadowolfbusiness@gmail.com>
2018-09-03 09:37:09 +1000

    Add automated channel creation on guild join.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,4 +3,7 @@ const config = require('../config.json');
 
 module.exports = guild => {
   console.log(`\n\n[Console] Joined the Guild ${guild.name}.\nGuild Owner: ${guild.owner.user.tag}\nNumber of Members: ${guild.memberCount}\nGuild Location: ${guild.region}\n\n`);
+  
+  guild.createChannel(`audit-log`, `text`).catch(console.error);
+  guild.createChannel(`reports`, `text`).catch(console.error);
 };
