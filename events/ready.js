f76c99e931f0d27c67185b8e77b151dc0ad88e63
shadowolf <shadowolfbusiness@gmail.com>
2018-09-05 11:09:54 +1000

    Reverting changes

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,7 @@ const token = require('../token.json');
 const config = require('../config.json');
 const superagent = require('superagent');
 
-module.exports = client => {
-  
-  client.emit("guildMemberAdd", guild);
-  
+module.exports = client => {  
   let pluralnonpluralservers = (client.guilds.size > 1) ? 'Servers' : 'Server';
   let pluralnonpluralusers = (client.users.size > 1) ? 'Users' : 'User';
 
