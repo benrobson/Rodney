5d59f884ff04fede0fdf2d416e68441ce538936a
shadowolfyt <shadowolfbusiness@gmail.com>
2018-10-24 14:21:09 +1100

    Added the ability to display a users Twitter followers.

--- events/ready.js
+++ events/ready.js
@@ -3,31 +3,35 @@ const config = require('../config.json');
 const superagent = require('superagent');
 const chalk = require('chalk');
 
-module.exports = client => {
+module.exports = async client => {
   let pluralnonpluralservers = (client.guilds.size > 1) ? 'Servers' : 'Server';
   let pluralnonpluralusers = (client.users.size > 1) ? 'Users' : 'User';
 
-  console.log(`\n\n${client.user.username} is online.\nOperating on ${client.guilds.size} ${pluralnonpluralservers}.\nOperating for ${client.users.size} ${pluralnonpluralusers}.\n\n`);
+  console.log(`\n\n${client.user.username} is online.\nOperating on ${client.guilds.size} ${pluralnonpluralservers}.\nOperating for ${client.users.size} ${pluralnonpluralusers}.\n`);
   client.user.setActivity('Booting..');
 
+  // Grabs a users Twitter followers and displays them
+  // Use https://codeofaninja.com/tools/find-twitter-id to find your Twitter id
+
+  if (config.guildid == '450938867034619914') {
+      let {body} = await superagent
+      .get('https://api.twittercounter.com/?twitter_id=' + config.twitterid + '&apikey=' + token.twittercounterapikey);
+      client.channels.get(config.twitterchannel).setName(`${body.username} Followers: ${body.followers_current}`);
+      console.log(chalk.green('[Console] ') + `Twitter Channel detected! Setting ammount of followers that @${body.username} has.`);
+      return;
+    } else {
+      return;
+    }
+  }
+
   function setActivity() {
+    // Sets Activity in a rotation
     const Gameinfo = [`Using ${(((process.memoryUsage().heapUsed)/1024)/1024).toFixed(0)}MBs of RAM`, 'Source: http://bit.ly/rodneybotsource', 'Developer: shadowolf#9212', 'Discord: http://bit.ly/mancavediscord', 'Invite: http://bit.ly/inviterodney', `Running on ${client.guilds.size} ${pluralnonpluralservers}`, `Running for ${client.users.size} ${pluralnonpluralusers}`, `Use ${config.prefix}help`];
     var info = Gameinfo[Math.floor(Math.random() * Gameinfo.length)];
 
     client.user.setActivity(info);
     console.log(chalk.green('[Console]') + ` Activity set to (${info})`);
   };
-
-/*
-Search for how many youtube subscribers the user has.
-
-const channel_id = 'UC23H6E9ZnXtBKbTuitExc5Q';
-const your_key = '';
-let {body} = superagent
-.get(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channel_id}&key=${your_key}`);
-
-console.log(body.items);
-*/
 
 setInterval(setActivity, 120000);
 };
