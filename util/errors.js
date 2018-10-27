3e8782f3d76ca1ea44d92241fe412d4330a24bd1
Aidan <a.jruiz1945@gmail.com>
2018-10-27 12:54:33 -0400

    Modifications to mute/unmute commands

--- util/errors.js
+++ util/errors.js
@@ -191,11 +191,21 @@ module.exports.anotherNumber = (message) => {
   message.channel.send(embed);
 };
 
-// Used if a user attempts to send a tweet command that is not in the #tweet channel
+// Used if a user attempts to mute a user who is not muted.
 module.exports.userNotMuted = (message) => {
   let embed = new Discord.RichEmbed()
   .setTitle('An error has occurred!')
   .setDescription('This user is not muted.')
+  .setColor(config.red);
+
+  message.channel.send(embed);
+};
+
+// Used if a user attempts to mute a user who is already muted
+module.exports.userAlreadyMuted = (message) => {
+  let embed = new Discord.RichEmbed()
+  .setTitle('An error has occurred!')
+  .setDescription('This user has already been muted.')
   .setColor(config.red);
 
   message.channel.send(embed);
@@ -236,46 +246,6 @@ module.exports.emptyCode = (message) => {
   let embed = new Discord.RichEmbed()
   .setTitle('An error has occurred!')
   .setDescription('Please specify the code you would like to send.')
-  .setColor(config.red);
-
-  message.channel.send(embed);
-};
-
-// Used if a user attempts to assign a role that does not exist
-module.exports.noRoleExists = (message) => {
-  let embed = new Discord.RichEmbed()
-  .setTitle('An error has occurred!')
-  .setDescription('The specified role does not exist.')
-  .setColor(config.red);
-
-  message.channel.send(embed);
-};
-
-// Used if a user is trying to be assigned a role that they already have
-module.exports.userHasRole = (message) => {
-  let embed = new Discord.RichEmbed()
-  .setTitle('An error has occurred!')
-  .setDescription('The specified user already has this role.')
-  .setColor(config.red);
-
-  message.channel.send(embed);
-};
-
-// Used if a user is trying to be assigned a role that they don't have
-module.exports.userDoesNotHaveRole = (message) => {
-  let embed = new Discord.RichEmbed()
-  .setTitle('An error has occurred!')
-  .setDescription('The specified user does not have this role.')
-  .setColor(config.red);
-
-  message.channel.send(embed);
-};
-
-// Used if a user is trying to be assigned a role that they don't have
-module.exports.specifyARole = (message) => {
-  let embed = new Discord.RichEmbed()
-  .setTitle('An error has occurred!')
-  .setDescription('Please specify a role to assign to the user.')
   .setColor(config.red);
 
   message.channel.send(embed);
