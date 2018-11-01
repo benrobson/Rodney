44c7fcd4e22d810e0b2bd6b89077b224b31ee350
Aidan <a.jruiz1945@gmail.com>
2018-11-01 16:54:08 -0400

    Missing chalk requirement

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
+const chalk = require('chalk');
 
 module.exports = async guild => {
   console.log(`\n\n` + chalk.green('[Console]') + ` Joined the Guild ${guild.name}.\nGuild Owner: ${guild.owner.user.tag}\nNumber of Members: ${guild.memberCount}\nGuild Location: ${guild.region}\n\n`);
