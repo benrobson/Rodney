4b4f445f03944849ebab9cf6673020ae3ca4c1db
shadowolf <shadowolfbusiness@gmail.com>
2019-03-09 19:06:31 +1100

    Fixed an error in command.

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,9 +4,9 @@ module.exports.run = (client, message, args) => {
   if (!args) return message.reply('Sorry, but you must specify what you want to set the custom server prefix as.');
   client.prefixes.set(message.guild.id, args);
   const embed = new Discord.RichEmbed()
-    .setTitle('Prefix Set!');
+    .setTitle('Prefix Set!')
     .setDescription(`The server custom prefix has been set to ${args}`)
-    .setColor('RANDOM')
+    .setColor('RANDOM');
   return message.channel.send(embed);
 }
 
